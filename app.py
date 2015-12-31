ab9230fbe5cd7720fec78ea60f11a46e7e1bf0e9
rocktronica <tommy@iamnotagoodartist.com>
2015-12-30 19:50:12 -0800

    Add "currently playing" metadata

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import flask
 import glob
+import json
 import os
 import subprocess
+import urllib2
 
 app = flask.Flask(__name__, template_folder='')
 
@@ -10,9 +12,18 @@ def action(script):
     os.system('sh action/' + script + '.sh')
     return 'ok'
 
+@app.route('/metadata')
+def get_metadata(jsonify=True):
+    url = 'http://www.thecurrent.org/playlist/metadata/current'
+    response = json.load(urllib2.urlopen(url))
+
+    return flask.jsonify(response) if jsonify else response
+
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html',
+        metadata = get_metadata(False)
+    )
 
 if __name__ == '__main__':
     app.debug = True
