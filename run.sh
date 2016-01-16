547bbfe85146aab61463919cbd7ce396941a15b7
rocktronica <tommy@iamnotagoodartist.com>
2016-01-15 22:56:02 -0800

    Prevent runner locally

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "`hostname`" != 'curpi' ]; then
+    echo "Run this on curpi machine, not locally"
+    exit 1
+fi
+
 screen -S curpi_app -dm bash -c 'while true; do
     sudo python app.py
     sleep 5
