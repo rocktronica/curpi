e1a0c723ca74b98ede85c882a2cde47a9a9ceb5f
rocktronica <tommy@iamnotagoodartist.com>
2016-01-16 00:00:58 -0800

    Fetch and use app active status

--- hardware.py
+++ hardware.py
@@ -5,6 +5,15 @@ import subprocess
 import time
 import urllib2
 import sys
+import threading
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    timer = threading.Timer(sec, func_wrapper)
+    timer.start()
+    return timer
 
 def hit_action_endpoint(action, arguments):
     host = arguments.host
@@ -31,6 +40,22 @@ def get_active_status(arguments):
 
     return result['active']
 
+def light_toggle(on):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.output(11, GPIO.HIGH if on else GPIO.LOW)
+
+def light_on():
+    light_toggle(True)
+
+def light_off():
+    light_toggle(False)
+
+def get_pressed():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.IN)
+    return GPIO.input(7)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default='0.0.0.0')
@@ -38,28 +63,36 @@ if __name__ == '__main__':
     parser.add_argument("--debug", action="store_true")
     arguments = parser.parse_args()
 
-    GPIO.setmode(GPIO.BOARD)
+    active = get_active_status(arguments)
+    pressed = get_pressed()
 
-    GPIO.setup(11, GPIO.OUT)
-    GPIO.setup(7, GPIO.IN)
+    def main():
+        global active, pressed
 
-    on = get_active_status(arguments)
-    pressed = False
-
-    while True:
         previously_pressed = pressed
-        pressed = GPIO.input(7)
+        pressed = get_pressed()
 
-        GPIO.output(11, GPIO.HIGH if on else GPIO.LOW)
+        light_toggle(active)
 
         if pressed and not previously_pressed:
-            on = not on
-            hit_action_endpoint('play' if on else 'stop', arguments)
+            active = not active
+            hit_action_endpoint('play' if active else 'stop', arguments)
 
         if arguments.debug:
-            print ("pressed" if GPIO.input(7) else "unpressed") + "\t" + \
-                ('on' if on else 'off')
+            print "main \t" + \
+                ('pressed' if GPIO.input(7) else 'unpressed') + "\t" + \
+                ('active' if active else 'off')
 
-        time.sleep(0.05)
+    set_interval(main, 0.05)
+
+    def fetch_active_status():
+        global active
+        active = get_active_status(arguments)
+
+        if arguments.debug:
+            print "fetch_active_status \t" + \
+                ('active' if active else 'off')
+
+    set_interval(fetch_active_status, 5)
 
     GPIO.cleanup()
