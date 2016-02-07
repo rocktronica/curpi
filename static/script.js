5489085ec848021d0a776682a763c26e27d9da76
rocktronica <tommy@iamnotagoodartist.com>
2016-02-07 13:37:08 -0800

    Tidy
    - <br> -> CSS
    - make JS less spaghetti

--- static/script.js
+++ static/script.js
@@ -1,42 +1,65 @@
-$("[data-action]").on("click", function() {
-    var $button = $(this),
-        action = $button.data("action");
+(function(exports, window, undefined) {
 
-    $.get("/action/" + action, function(resp){
-        update();
+exports.IndexView = function($el) {
+    var view = this;
+
+    var _updating = null;
+
+    $el.find("[data-action]").on("click", function() {
+        var $button = $(this),
+            action = $button.data("action");
+
+        // TODO: don't need to do this every single time, especially since
+        // player may not immediately be finished
+        $.get("/action/" + action, function(resp){
+            view.update();
+        });
     });
-});
 
-var update = function(statusData) {
-    if (!statusData) {
-        $.getJSON("/status", update);
-        return;
-    }
-
-    $("[data-metadata]").toggle(statusData.active);
-
-    $("[data-action]").removeClass("active");
-    $("[data-action='" + (statusData.active ? "play" : "stop") + "']")
-        .addClass("active")
-
-    var metadata = statusData.metadata;
-    if (!metadata) { return; }
-
-    $("[data-metadata]").each(function() {
-        var $element = $(this),
-            key = $element.data("metadata"),
-            fill = $element.data("metadataFill"),
-            value = metadata[key];
-
-        switch (fill) {
-            case("text"):
-                $element.text(value);
-                break;
-            case("src"):
-                $element
-                    .attr("src", value || "")
-                    .toggle(!!value);
-                break;
+    view.update = function(statusData) {
+        if (!statusData) {
+            $.getJSON("/status", view.update);
+            return view;
         }
-    });
+
+        $el.find("[data-metadata]").toggle(statusData.active);
+
+        var currentActionElement =
+            "[data-action='" + (statusData.active ? "play" : "stop") + "']";
+        $el.find("[data-action]")
+            .removeClass("active")
+            .filter(currentActionElement)
+            .addClass("active")
+
+        var metadata = statusData.metadata;
+        if (!metadata) { return view; }
+
+        $el.find("[data-metadata]").each(function() {
+            var $element = $(this),
+                key = $element.data("metadata"),
+                fill = $element.data("metadataFill"),
+                value = metadata[key];
+
+            switch (fill) {
+                case("text"):
+                    $element.text(value);
+                    break;
+                case("src"):
+                    $element
+                        .attr("src", value || "")
+                        .toggle(!!value);
+                    break;
+            }
+        });
+
+        return view;
+    };
+
+    view.autoUpdate = function(seconds) {
+        _updating = window.setInterval(view.update, seconds * 1000);
+    };
+
+    return view;
 };
+
+}(this, window));
