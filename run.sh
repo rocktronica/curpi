a8a4c06d4d234f1bc9b4be417ffddd38f0d597c5
rocktronica <tommy@iamnotagoodartist.com>
2016-03-06 16:54:03 -0800

    Pass runner arguments

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# TODO: run on boot via /etc/rc.local, fix user mismatch errors (?), and
-# pass extra arguments
+# TODO: run on boot via /etc/rc.local, fix user mismatch errors (?)
 # sudo -H -u tommy bash -c 'sh /home/tommy/curpi/run.sh &'
 
 {
 
 dir=$(dirname $0)
+arguments="$*"
 
 if [ "`hostname`" != 'curpi' ]; then
     echo "Run this on curpi machine, not locally"
@@ -17,12 +17,12 @@ screen -S app -X quit
 screen -S hardware -X quit
 
 screen -S app -dm bash -c "while true; do
-    sudo python $dir/app.py
+    sudo python $dir/app.py $arguments
     sleep 5
 done"
 
 screen -S hardware -dm bash -c "while true; do
-    sudo python $dir/hardware.py
+    sudo python $dir/hardware.py $arguments
     sleep 5
 done"
 
